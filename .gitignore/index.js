945684f4a83dbe82ace1aca23308024be6f75294
Naash-Rah <44710176+Naash-Rah@users.noreply.github.com>
2018-11-10 22:08:50 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -57,11 +57,11 @@ else if (message.content.startsWith("!météo")){
     
     try {
         weather.find({search: location, degreeType: unit}, function(err, data) {
-            if(err) {
-                console.log(Date.now(), "DANGER", "Je ne peut pas trouvé d'information pour la méteo de " + location);
-                message.reply("\n" + "Je ne peut pas trouvé d'information pour la méteo de " + location);
+            if(data.length === 0){
+                console.log(Date.now(), "DANGER", "\nJe ne peut pas trouvé d'information pour la méteo de cette localisation.");
+                message.reply("\n" + "Je ne peut pas trouvé d'information pour la méteo de cette localisation.");
             } else {
-                data = data[0];
+               const forecast = data[0];
                console.log("\n\n** " + data.location.name + " - Maintenant **\n" + data.current.temperature + "°" + unit + " " + data.current.skytext + ", ressentie " + data.current.feelslike + "°, " + data.current.winddisplay + " Vent\n\n** Prévisions pour demain **\nHaut: " + data.forecast[1].high + "°, Bas: " + data.forecast[1].low + "° " + data.forecast[1].skytextday + " avec " + data.forecast[1].precip + "% de chance de precipitation.");
                message.reply("\n" + "**" + data.location.name + "\nMaintenant : **\n" + data.current.temperature + "°" + unit + " " + data.current.skytext + ", ressentie " + data.current.feelslike + "°, " + data.current.winddisplay + " Vent\n\n**Prévisions pour demain :**\nHaut: " + data.forecast[1].high + "°, Bas: " + data.forecast[1].low + "° " + data.forecast[1].skytextday + " avec " + data.forecast[1].precip + "% de chance de precipitation.");
             }
